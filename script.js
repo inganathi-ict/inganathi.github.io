5d2698fb4df30356b8de9b18547b12f19b166c5a
Inganathi ICT Solutions <inganathict@gmail.com>
2026-03-16 20:27:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ function sendMail(){
     message : document.getElementById("message").value,
   }
 
-
+  emailjs.send("service_l9q3vhf","template_ukf6vcb",parms).then(alert("Email has been sent"))
 }
