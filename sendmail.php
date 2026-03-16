124a0314f9ff7e6b8f1ae1da4e95704bd55c2b68
Inganathi ICT Solutions <inganathict@gmail.com>
2026-03-16 13:27:00 +0200

    sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -4,16 +4,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 $name = htmlspecialchars($_POST['name']);
 $email = htmlspecialchars($_POST['email']);
-$phone = htmlspecialchars($_POST['phone']);
 $message = htmlspecialchars($_POST['message']);
 
-$to = "your@email.com";   // your email address
-$subject = "New Website Enquiry";
+$to = "duncanmooko@outlook.com";   // your email address
+$subject = "Inganathi Enquiry";
 
 $body = "You have received a new enquiry from your website.\n\n";
 $body .= "Name: ".$name."\n";
 $body .= "Email: ".$email."\n";
-$body .= "Phone: ".$phone."\n\n";
 $body .= "Message:\n".$message;
 
 $headers = "From: ".$email;
